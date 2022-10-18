1bd7006462143556dfd37dacdfed3fd9f0a66e47
gasperpb <achillespb@hotmail.com>
2022-10-18 20:34:47 -0300

    criado o form com a função add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { useRef, useState } from "react";
+
 function App() {
-  return "hi";
+  const [items, setItems] = useState([]);
+  const inputRerf = useRef();
+
+  function onSubmit(e) {
+    e.preventDefault();
+    const value = inputRerf.current.value;
+    if (value === "") return;
+    setItems((prev) => {
+      return [...prev, value];
+    });
+    inputRerf.current.value = "";
+  }
+  return (
+    <>
+      Search
+      <input type="search" />
+      <br />
+      <br />
+      <form onSubmit={onSubmit}>
+        New Item: <input ref={inputRerf} type="text" />
+        <button type="submit">Add</button>
+      </form>
+      <h3>Items</h3>
+      {items.map((item) => (
+        <div>{item}</div>
+      ))}
+    </>
+  );
 }
 
 export default App;
